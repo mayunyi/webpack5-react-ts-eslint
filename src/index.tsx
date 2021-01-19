6a5c709e211c81ce0c86100734fffb7b9fd06061
马海 <mahai@darthcloud.com>
2021-01-19 15:45:59 +0800

    test

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,7 @@ import './index.scss';
 
 const Index = () => {
     return <div className='a'> hello, world 333 </div>;
+
 };
 export default hot(Index);
 ReactDom.render(<Index />, document.getElementById('root'));
